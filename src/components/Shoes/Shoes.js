2d7c169533e2438a43bf609aa119e3ca442cdc4f
md-abir-alahe <mdabiralahe@gmail.com>
2023-03-30 02:32:50 +0600

    added

---
+++ src/components/Shoes/Shoes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { add, multiply } from '../../utilities/calculate';
+
+const Shoes = () => {
+    const first = 13;
+    const second = 11;
+    const result = multiply(first, second);
+    const sum = add(first, second);
+    return (
+        <div>
+            <h3>This is show components</h3>
+            <p>Result : {result}</p>
+            <p>Sum : {sum}</p>
+        </div>
+    );
+};
+
+export default Shoes;
